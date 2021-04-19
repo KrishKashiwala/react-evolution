39dfc80c2827516beb4c51bbda4f87c79463e8f8
Krish Kashiwala <69708017+KrishKashiwala@users.noreply.github.com>
2021-04-19 17:41:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import './App.css';
+// import FRParentInput from './components/FRParentInput';
+import PortalDemo from './components/PortalDemo';
+// import FocusInput from './components/FocusInput';
+// import RefsDemo from './components/RefsDemo'; 
+// import MemoComp from './components/MemoComp';
+// import ParentComp from './components/ParentComp';
+// import PureComp from './components/PureComp';
+// import FragmentDemo from './components/FragmentDemo';
+// import Table from './components/Table';
+// import StyleSheet from './components/StyleSheet';
+// import './appStyles.css'
+// import styles from './appStyles.module.css'
+// import Forms from './components/Forms';
+// import LifecycleA from './components/LifecycleA';
+// import NameList from './components/NameList';
+// import UserGreeting from './components/UserGreeting';
+// import ParentComponent from './components/ParentComponent';
+
+// import ClassClick from './components/ClassClick';
+// import EventBind from './components/EventBind';
+// import FunctionComponent from './components/FunctionComponent';
+// import Welcome from './components/Welcome'
+// import Greet from './components/Greet'
+// import Hello from './components/Hello'
+// import Message from './components/Message'
+// import Counter from './components/Counter'
+
+const App = () =>{
+  return (
+    <div className="App">
+          {/* <Greet name = "Krish Kashiwala">
+            <p>this is cool</p>
+          </Greet>
+          <Welcome />
+          <Hello/> */}
+          {/* <Message /> */}
+          {/* <Counter/> */}
+          {/* <FunctionComponent/> */}
+          {/* <ClassClick/> */}
+          {/* <EventBind/> */}
+          {/* <ParentComponent/> */}
+          {/* <UserGreeting/> */}
+          {/* <NameList/> */}
+          {/* <StyleSheet/>
+          <h1 className = "error">Error</h1>
+          <h1 className = {styles.success}>Success</h1> */}
+          {/* <Forms/> */}
+          {/* <LifecycleA/> */}
+          {/* <FragmentDemo/> */}
+          {/* <Table/> */}
+          {/* <PureComp/> */}
+          {/* <ParentComp/> */}
+          {/* <MemoComp/> */}
+          {/* <RefsDemo/> */}
+          {/* <FocusInput/> */}
+          {/* <FRParentInput/> */}
+          <PortalDemo/>
+    </div>
+  );
+}
+
+export default App;
